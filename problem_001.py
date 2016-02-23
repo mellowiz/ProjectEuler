2d9f78125c0ac3f81e5ee8a511a5dc437e258c97
Mel Lowiz <mellowiz@hotmail.com>
2016-02-23 13:55:25 +0100

    Added some inline comments

--- problem_001.py
+++ problem_001.py
@@ -5,9 +5,9 @@ from __future__ import division, print_function
 maxRange = raw_input('Enter upper range limit: ')
 maxRangeN = int(maxRange)
 
-L = []
+L = []						# initialize empty list
 for i in range(1,maxRangeN):
-  if (i // 3 == i / 3) or (i // 5 == i / 5):
+  if (i // 3 == i / 3) or (i // 5 == i / 5):	# only add to L multiples of 3 or 5
     L.append(i)
 
 print("List of numbers between 1 and %d:" % maxRangeN)
--- problem_002.py
+++ problem_002.py
@@ -9,9 +9,9 @@ i = 0
 
 while True:
   print("iteration: %d, value A = %d, value B = %d, Sum = %d" % (i, a, b, Sum))
-  a, b = b, a + b
+  a, b = b, a + b			# fib(n) = fib(n-1) + fib(n-2)
   if a >= MaxN: break
-  if a % 2 == 0:
+  if a % 2 == 0:			# only add even numbers
     Sum += a
   i += 1
 
